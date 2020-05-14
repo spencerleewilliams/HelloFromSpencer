5d469e1852a43d5948a1843a3b503e8c6e9a2948
Hannah Rogers <rog19008@byui.edu>
2020-05-14 16:10:34 -0600

    Revert "Hello from Hannah"
    This reverts commit 074f9ff6963b29175496ae6c323598d010c7c3b8.

--- src/williams/Main.java
+++ src/williams/Main.java
@@ -3,6 +3,5 @@ package williams;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello my name is Spencer.");
-        System.out.println("Hello Spencer, my name is Hannah");
     }
 }
